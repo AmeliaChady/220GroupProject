0e71c0a5f8a39523440e11448e2a95b40509f519
Amelia Chady <achadu@ithaca.edu>
2018-11-27 15:06:11 -0500

    added docstrings to Sound.h

--- Sound.h
+++ Sound.h
@@ -10,18 +10,45 @@
 class Sound{
 private:
     std::string symbol;
+    //List<Sound> connectedSounds;
 
     Sound(const Sound& soundToCopy);
     Sound& operator=(const Sound& soundToCopy);
     Sound();
+
+    /**
+     * Connects a Sound to this sound.
+     * @param connectedSound the sound to connect to.
+     */
+    void AddConnectionSecondary(Sound& connectedSound);
 public:
+    /**
+     * Constructs a Sound Object
+     * @param symbol the symbol representing the sound object
+     */
     Sound(std::string symbol);
 
+    /**
+     * Connects the two sound objects together
+     * @param connectedSound the other sound to connect to
+     */
     void AddConnection(Sound& connectedSound);
 
+    /**
+     * Randomly gets a similar sound
+     * @return The symbol of that Sound
+     */
     std::string getSimilar();
+    /**
+     * Randomly gets a similar sound
+     * @return that Sound
+     */
     Sound getSimilarSound();
 
+    /**
+     * Get the symbol of this Sound
+     * @return the symbol
+     */
     std::string getSymbol();
 };
 
