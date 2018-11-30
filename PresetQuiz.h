f85cda5104ad717eb344444ef9d5fe815da3a5c3
Timothy Marotta <timothymarotta@Timothys-MacBook-Pro.local>
2018-11-30 01:04:35 -0500

    Tried my best to get quiz interfaces working, hard to say when I can't see Question or ListOfWords

---
+++ PresetQuiz.h
@@ -0,0 +1,15 @@
+//
+// Created by Timothy Marotta on 11/30/18.
+//
+
+#ifndef INC_220GROUPPROJECT_PRESETQUIZ_H
+#define INC_220GROUPPROJECT_PRESETQUIZ_H
+
+
+#include "Quiz.h"
+
+class PresetQuiz: public Quiz {
+
+};
+
+#endif //INC_220GROUPPROJECT_PRESETQUIZ_H
--- Quiz.h
+++ Quiz.h
@@ -5,4 +5,28 @@
 #ifndef INC_220GROUPPROJECT_QUIZ_H
 #define INC_220GROUPPROJECT_QUIZ_H
 
+#include <string>
+
+class Quiz {
+private:
+    /*
+     * asks for Question object to be made
+     * @returns Question object temp void
+     */
+    virtual void makeQuestion(std::string word)=0;
+
+public:
+    //destructor to be declared in child classes
+    virtual ~Quiz()=0;
+
+    /*
+     * @input takes word passed from ListOfWords
+     * @returns Question object (currently void bc class doesn't exist yet
+     *
+     */
+    virtual void genQuestion(std::string word)=0;
+
+
+
+};
 #endif //INC_220GROUPPROJECT_QUIZ_H
