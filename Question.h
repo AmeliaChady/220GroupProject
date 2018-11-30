376056bec62d4df3819097574757307037251287
JoshHayden <jhayden@ithaca.edu>
2018-11-30 11:02:14 -0500

    Hopefully this worked lol

--- Question.h
+++ Question.h
@@ -14,9 +14,10 @@ private:
     std::string wrongOne;
     std::string wrongTwo;
     std::string wrongThree;
+    std::string stringEdit(std::string toEdit, int index, std::string toInsert);
 public:
     ~Question();
-    Question(std::pair<std::string,std::string>); //generates a new question, uses the soundMap to generate 3 random incorrect questions
+    Question(std::pair<std::string,std::string> word); //generates a new question, uses the soundMap to generate 3 random incorrect questions
     //the pair here is output from wordList originally, first string is the english word and second string is the correct phonetic translation
     void ask(); //gives the use a multiple choice question, tells them if their answer is correct or not
     std::string outputQuestion(); //outputs the question in a printout type of format, not sure if we need this functionality
