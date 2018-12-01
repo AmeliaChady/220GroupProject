9006e349392988503f8be793aee14fa193d3e8c4
Amelia Chady <achadu@ithaca.edu>
2018-12-01 15:38:56 -0500

    continued work on Util and Util Testing

--- UtilTesting.cpp
+++ UtilTesting.cpp
@@ -10,22 +10,69 @@ void trimTest(){
     int passes = 0;
     int fails = 0;
 
-    // Test 1
     if(trim("  a") == "a"){
-
+        std::cout << "passed" << std::endl;
+        passes++;
     }else{
-
+        std::cout << "failed" << std::endl;
+        fails++;
     }
 
+    if(trim("a  ") == "a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
 
+    if(trim("  a  ") == "a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
 
-    // Test 2
+    if(trim("\r  a  \r") == "a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
 
+    if(trim("\n  a  \n") == "a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
 
-    // Test 3
+    if(trim(" a a  ") == "a a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
 
+    if(trim(" ") == ""){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
 
-    //
+    if(trim("") == ""){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
 
 
     std::cout << "Overall:" << std::endl;
@@ -34,12 +81,81 @@ void trimTest(){
     std::cout << "--Done--------" << std::endl;
 }
 
+//TODO: Finish tests
 void splitTest(){
+    std::cout << "--Split Tests--------" << std::endl;
+    int passes = 0;
+    int fails = 0;
 
+    std::string testString = " a b c ";
+    List<std::string>* list = split(testString, " ");
+    if(list->getValueAt(0)=="a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
+
+    testString = "0a0b0c0";
+    delete list;
+    list = split(testString, "0");
+    if(list->getValueAt(0)=="a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
+
+    testString = "01a01b01c01";
+    delete list;
+    list = split(testString, "01");
+    if(list->getValueAt(0)=="a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
+
+    testString = "abc";
+    delete list;
+    list = split(testString, "");
+    if(list->getValueAt(0)=="abc"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
+
+    testString = "01a01b01c01";
+    delete list;
+    list = split(testString, "01");
+    if(list->getValueAt(0)=="a"){
+        std::cout << "passed" << std::endl;
+        passes++;
+    }else{
+        std::cout << "failed" << std::endl;
+        fails++;
+    }
+
+
+
+
+
+    std::cout << "Overall:" << std::endl;
+    std::cout << "Passed " << passes << " times." << std::endl;
+    std::cout << "Failed " << fails << " times." << std::endl;
+    std::cout << "--Done--------" << std::endl;
 }
 
 int main(){
-    std::string lazyTest = "    a b c  d    \n";
+    trimTest();
+    splitTest();
+
+    /*std::string lazyTest = "    a b c  d    \n";
     std::string lazyTest2 = trim(lazyTest);
     List<std::string>* lazyTest3 = split(lazyTest2, " ");
 
@@ -54,7 +170,7 @@ int main(){
     std::cout << "]" << std::endl;
 
     std::cout << "DONE" << std::endl;
-    delete lazyTest3;
+    delete lazyTest3;*/
 
     return 0;
 }
