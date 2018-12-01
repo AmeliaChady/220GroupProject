5f9917dba4d3ceba29cfe3827e128848929559b2
JoshHayden <jhayden@ithaca.edu>
2018-12-01 15:31:25 -0500

    New updates, changes to Question

--- Question.cpp
+++ Question.cpp
@@ -5,15 +5,17 @@
 #include "Question.h"
 #include "ArraySoundMap.h"
 
-Question::~Question(){
+
+std::string stringEdit(std::string toEdit, int index, std::string toInsert){
+    std::string newString = toEdit;
+    newString[index] = toInsert;
+    return stringEdit;
 
 }
 
-std::string stringEdit(std::string toEdit, int index, std::string toInsert);
-
 //generates a new question, uses the soundMap to generate 3 random incorrect questions
 //the pair here is output from wordList originally, first string is the english word and second string is the correct phonetic translation
-Question::Question(std::pair<std::string,std::string> word){
+Question::Question(std::pair<std::string,std::string> word, ArraySoundMap* soundMap){
     this.englishWord = std::get<0>(word);
     this.correctAnswer = std::get<1>(word);
     std::string baseString = "Does this work lol";
@@ -22,10 +24,82 @@ Question::Question(std::pair<std::string,std::string> word){
 }
 //gives the use a multiple choice question, tells them if their answer is correct or not
 void Question::ask(){
-
+    int order = rand() % 4;
+    if (order == 0){
+        std::cout<<englishWord<<std::endl;
+        std::cout<<"A. " + correctAnswer<<std::endl;
+        std::cout<<"B. " + wrongOne<<std::endl;
+        std::cout<<"C. " + wrongTwo<<std::endl;
+        std::cout<<"D. " + wrongThree<<std::endl;
+        std::string userAnswer;
+        std::cin >> userAnswer;
+        if (userAnswer == "A"){
+            response = true;
+            std::cout<< "Correct" << std::endl;
+        }
+        else{
+            response = false;
+            std::cout<< "Incorrect" <<std::endl;
+        }
+    }
+    if (order == 1){
+        std::cout<<englishWord<<std::endl;
+        std::cout<<"A. " + wrongOne<<std::endl;
+        std::cout<<"B. " + correctAnswer<<std::endl;
+        std::cout<<"C. " + wrongTwo<<std::endl;
+        std::cout<<"D. " + wrongThree<<std::endl;
+        std::string userAnswer;
+        std::cin >> userAnswer;
+        if (userAnswer == "B"){
+            response = true;
+            std::cout<< "Correct" << std::endl;
+        }
+        else{
+            response = false;
+            std::cout<< "Incorrect" <<std::endl;
+        }
+    }
+    if (order == 2){
+        std::cout<<englishWord<<std::endl;
+        std::cout<<"A. " + wrongOne<<std::endl;
+        std::cout<<"B. " + wrongTwo<<std::endl;
+        std::cout<<"C. " + correctAnswer<<std::endl;
+        std::cout<<"D. " + wrongThree<<std::endl;
+        std::string userAnswer;
+        std::cin >> userAnswer;
+        if (userAnswer == "C"){
+            response = true;
+            std::cout<< "Correct" << std::endl;
+        }
+        else{
+            response = false;
+            std::cout<< "Incorrect" <<std::endl;
+        }
+    }
+    if (order == 3){
+        std::cout<<englishWord<<std::endl;
+        std::cout<<"A. " + wrongOne<<std::endl;
+        std::cout<<"B. " + wrongTwo<<std::endl;
+        std::cout<<"C. " + wrongThree<<std::endl;
+        std::cout<<"D. " + correctAnswer<<std::endl;
+        std::string userAnswer;
+        std::cin >> userAnswer;
+        if (userAnswer == "D"){
+            response = true;
+            std::cout<< "Correct" << std::endl;
+        }
+        else{
+            response = false;
+            std::cout<< "Incorrect" <<std::endl;
+        }
+    }
 }
 //outputs the question in a printout type of format, not sure if we need this functionality
 //but should be easy enough to do, basically a toString
 std::string Question::outputQuestion(){
 
+}
+
+bool Question::getResponse() {
+    return response;
 }
--- Question.h
+++ Question.h
@@ -8,20 +8,20 @@
 
 class Question{
 private:
-    ArraySoundMap soundMap;
     std::string englishWord; //english translation
     std::string correctAnswer;
     std::string wrongOne;
     std::string wrongTwo;
     std::string wrongThree;
     std::string stringEdit(std::string toEdit, int index, std::string toInsert);
+    bool response; //tracks if the user got the question wrong or right
 public:
-    ~Question();
-    Question(std::pair<std::string,std::string> word); //generates a new question, uses the soundMap to generate 3 random incorrect questions
+    Question(std::pair<std::string,std::string> word, ArraySoundMap* soundMap); //generates a new question, uses the soundMap to generate 3 random incorrect questions
     //the pair here is output from wordList originally, first string is the english word and second string is the correct phonetic translation
     void ask(); //gives the use a multiple choice question, tells them if their answer is correct or not
     std::string outputQuestion(); //outputs the question in a printout type of format, not sure if we need this functionality
     //but should be easy enough to do, basically a toString
+    bool getResponse(); //outputs response
 
 };
 #endif //INC_220GROUPPROJECT_QUESTION_H
